da1b922645342c38be3aba6a482538d9dee66ffd
Gizmotronn <droidology@allianceofdroids.org.au>
2022-07-13 14:23:32 +0800

    📩🩸 -> Add free nft section for showcase

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,24 @@
 import React from 'react'
+import home from "../assets/home.png";
 
 export default function Home() {
-  return <div>Free</div>
+  return <div className='home'>
+    <div className='container'>
+      <div className='content'>
+        <p className='sub-title'>Launching soon</p>
+        <h1 className='title'>The Harambians</h1>
+        <p className='description'>Don't miss out on the release</p>
+        <button>Sign Up</button>
+      </div>
+      <div className='image-container'>
+        <div className='image'>
+          <img src={home} alt="home" />
+        </div>
+        <div className='ellipse-container'>
+          <div className='ellipse pink'></div>
+          <div className='ellipse orange'></div>
+        </div>
+      </div>
+    </div>
+  </div>;
 }
